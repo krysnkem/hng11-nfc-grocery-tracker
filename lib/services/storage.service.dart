14c686e8492911fb30e6b2e69d52475ad4176433
bolexyro <bolexyro@gmail.com>
2024-07-29 12:08:47 +0000

    Create providers

--- lib/services/storage.service.dart
+++ lib/services/storage.service.dart
@@ -1,26 +1,88 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+// so our storage would be for items and activity
+// for items, we
+// should be able to read all items
+// want to be able to create new items in the db,
+// update an item i.e reduce or increase the qty
+// and delete an item
+
+// for recent activity, we just want to
+// read all activities
+// write an activity
+
+import 'package:grocey_tag/core/enums/enum.dart';
+import 'package:grocey_tag/core/models/activity.dart';
+import 'package:grocey_tag/core/models/item.dart';
 
 class StorageService {
-  FlutterSecureStorage storage = const FlutterSecureStorage();
+  static const _operationDuration = Duration(milliseconds: 500);
 
-  storeItem({String? key, String? value}) async {
-    await storage.write(key: key!, value: value);
+  static Future<List<Item>> readAllItems() async {
+    try {
+      await Future.delayed(_operationDuration);
+      final itemNames = [
+        'Milk',
+        'Sausage',
+        'Bread',
+        'Fruits',
+        'Eggs',
+        'Mayonnaise',
+        'Pepper',
+      ];
+
+      return itemNames
+          .map(
+            (eachItem) => Item(
+                id: eachItem,
+                name: eachItem,
+                quantity: 10,
+                unit: Unit.litre,
+                purchaseDate: DateTime.now(),
+                expiryDate: DateTime(2024),
+                additionalNote: 'This is $eachItem'),
+          )
+          .toList();
+    } catch (e) {
+      rethrow;
+    }
   }
 
-  Future<dynamic> readItem({String? key}) async {
-    final value = await storage.read(key: key!);
-    return value;
+  static Future<void> registerItem(Item item) async {
+    try {
+      await Future.delayed(_operationDuration);
+    } catch (e) {
+      rethrow;
+    }
   }
 
-  deleteItem({String? key}) async {
-    await storage.delete(key: key!);
+  static Future<void> delete(Item item) async {
+    try {
+      await Future.delayed(_operationDuration);
+    } catch (e) {
+      rethrow;
+    }
   }
 
-  deleteAllItems() async {
-    await storage.deleteAll();
+  static Future<void> update(Item item) async {
+    await Future.delayed(_operationDuration);
   }
 
-  Future<dynamic> hasKey({String? key}) async {
-    return await storage.containsKey(key: key!);
+  // we are done with the operations for items
+
+  static Future<List<Activity>> readAllActivity() async {
+    await Future.delayed(_operationDuration);
+    return [
+      Activity(
+        itemName: 'Mr Beast Choco',
+        operation: Operation.add,
+        quantity: 5,
+        date: DateTime.now().subtract(
+          const Duration(days: 2),
+        ),
+      )
+    ];
+  }
+
+  static Future<void> registerActivity(Activity activity) async {
+    await Future.delayed(_operationDuration);
   }
 }
