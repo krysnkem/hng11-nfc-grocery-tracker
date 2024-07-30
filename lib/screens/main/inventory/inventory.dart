2b2fa27f708c01fbdc472de1764fe0a76bb20b65
Andima <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>
2024-07-30 19:02:06 +0100

    UI/Shop_fix

--- lib/screens/main/inventory/inventory.dart
+++ lib/screens/main/inventory/inventory.dart
@@ -8,78 +8,107 @@ import 'package:grocey_tag/core/constants/pallete.dart';
 import 'package:grocey_tag/utils/widget_extensions.dart';
 import 'package:grocey_tag/widgets/app_button.dart';
 import 'package:grocey_tag/widgets/apptext.dart';
-import 'package:grocey_tag/widgets/searchbar.dart';
 import 'package:grocey_tag/widgets/text-field-widget.dart';
 
 import '../../../widgets/filter.dart';
 import '../edit-item/edit-item-screen.dart';
 
-class Inventory extends StatelessWidget {
+class Inventory extends StatefulWidget {
   final Function(int) onNavigationItem;
   const Inventory({super.key, required this.onNavigationItem});
 
   @override
-  Widget build(BuildContext context) {
-    List<String> filterItems = ["Date Bought", "Quantity", "Expiry Date"];
+  State<Inventory> createState() => _InventoryState();
+}
 
-    String? selectedFilter;
+class _InventoryState extends State<Inventory> {
+  List<String> filterItems = ["Date Bought", "Quantity", "Expiry Date"];
 
-    onSelect(String val){
-      selectedFilter = val;
+  String? selectedFilter;
+
+  onSelect(String val) {
+    selectedFilter = val;
+  }
+
+  List<Map<String, dynamic>> filteredItem = [];
+
+  searchItems(String? val) {
+    if (val == null) {
+      filteredItem = item;
+    } else if (val.length == 0) {
+      filteredItem = item;
+    } else {
+      filteredItem = item.where((item) {
+        return item["title"]!.toLowerCase().contains(val.toLowerCase()) ||
+            item["purchase date"]!.toLowerCase().contains(val.toLowerCase()) ||
+            item["expiry date"]!.toLowerCase().contains(val.toLowerCase()) ||
+            item["measureUnit"]!.toLowerCase().contains(val.toLowerCase()) ||
+            item["quantity"]!.toString().contains(val);
+      }).toList();
     }
+    setState(() {});
+  }
 
-    List<Map<String, dynamic>> item = [
-      {
-        "title": "Bell Carrot",
-        "purchase date": "22 January 2024",
-        "expiry date": "22 Aug 2027",
-        "measureUnit": "KG",
-        "quantity": 5,
-      },
-      {
-        "title": "Melon Pods",
-        "purchase date": "22 January 2024",
-        "expiry date": "22 Aug 2027",
-        "measureUnit": "Cups",
-        "quantity": 10,
-      },
-      {
-        "title": "Green Peas",
-        "purchase date": "22 January  2024",
-        "expiry date": "22 Aug 2027",
-        "measureUnit": "KG",
-        "quantity": 2,
-      },
-      {
-        "title": "Fettucine Pasta",
-        "purchase date": "22 January  2024",
-        "expiry date": "22 Aug 2027",
-        "measureUnit": "Packs",
-        "quantity": 2,
-      },
-      {
-        "title": "Indomie",
-        "purchase date": "22 January  2024",
-        "expiry date": "22 Aug 2027",
-        "measureUnit": "Carton",
-        "quantity": 1,
-      },
-      {
-        "title": "Terylyaki",
-        "purchase date": "22 January  2024",
-        "expiry date": "22 Aug 2027",
-        "measureUnit": "litres",
-        "quantity": 4,
-      },
-      {
-        "title": "Japan Macha",
-        "purchase date": "22 January  2024",
-        "expiry date": "22 Aug 2027",
-        "measureUnit": "units",
-        "quantity": 2,
-      },
-    ];
+  List<Map<String, dynamic>> item = [
+    {
+      "title": "Bell Carrot",
+      "purchase date": "22 January 2024",
+      "expiry date": "22 Aug 2027",
+      "measureUnit": "KG",
+      "quantity": 5,
+    },
+    {
+      "title": "Melon Pods",
+      "purchase date": "22 January 2024",
+      "expiry date": "22 Aug 2027",
+      "measureUnit": "Cups",
+      "quantity": 10,
+    },
+    {
+      "title": "Green Peas",
+      "purchase date": "22 January  2024",
+      "expiry date": "22 Aug 2027",
+      "measureUnit": "KG",
+      "quantity": 2,
+    },
+    {
+      "title": "Fettucine Pasta",
+      "purchase date": "22 January  2024",
+      "expiry date": "22 Aug 2027",
+      "measureUnit": "Packs",
+      "quantity": 2,
+    },
+    {
+      "title": "Indomie",
+      "purchase date": "22 January  2024",
+      "expiry date": "22 Aug 2027",
+      "measureUnit": "Carton",
+      "quantity": 1,
+    },
+    {
+      "title": "Terylyaki",
+      "purchase date": "22 January  2024",
+      "expiry date": "22 Aug 2027",
+      "measureUnit": "litres",
+      "quantity": 4,
+    },
+    {
+      "title": "Japan Macha",
+      "purchase date": "22 January  2024",
+      "expiry date": "22 Aug 2027",
+      "measureUnit": "units",
+      "quantity": 2,
+    },
+  ];
 
+  @override
+  void initState() {
+    filteredItem = item;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     bool isempty = false;
     return Scaffold(
         appBar: AppBar(
@@ -128,104 +157,112 @@ class Inventory extends StatelessWidget {
                   ]))
                 ],
               )
-            :
-        GestureDetector(
-          onTap: ()=> FocusManager.instance.primaryFocus?.unfocus(),
-          child: Column(
-            children: [
-              Padding(
-                padding: 16.sp.padH,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            : GestureDetector(
+                onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+                child: Column(
                   children: [
-                    Expanded(
-                      child: AppTextField(
-                        contentPadding: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 7.sp),
-                        prefix: Icon(
-                          CupertinoIcons.search,
-                          size: 26.sp,
-                        ),
-                        hint: "Search",
+                    Padding(
+                      padding: 16.sp.padH,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: AppTextField(
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 16.sp, vertical: 7.sp),
+                              prefix: Icon(
+                                CupertinoIcons.search,
+                                size: 26.sp,
+                              ),
+                              hint: "Search",
+                              onChanged: searchItems,
+                            ),
+                          ),
+                          20.w.sbW,
+                          Container(
+                            height: 48.sp,
+                            width: 88.sp,
+                            alignment: Alignment.center,
+                            child: PopupMenuButton<String>(
+                              onSelected: onSelect,
+                              itemBuilder: (BuildContext context) {
+                                return filterItems.map((String choice) {
+                                  return PopupMenuItem<String>(
+                                    value: choice,
+                                    child: AppText(
+                                      choice,
+                                      weight: FontWeight.w400,
+                                      size: 12.sp,
+                                    ),
+                                  );
+                                }).toList();
+                              },
+                              child: const Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Filterbutton(),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
                       ),
                     ),
-                    20.w.sbW,
-                    Container(
-                      height: 48.sp,
-                      width: 88.sp,
-                      alignment: Alignment.center,
-                      child: PopupMenuButton<String>(
-                        onSelected: onSelect,
-                        itemBuilder: (BuildContext context) {
-                          return filterItems.map((String choice) {
-                            return PopupMenuItem<String>(
-                              value: choice,
-                              child: AppText(choice, weight: FontWeight.w400, size: 12.sp,),
-                            );
-                          }).toList();
+                    15.h.sbH,
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: filteredItem.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              // Navigator.push(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (context) => EditItemScreen()));
+                            },
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Row(
+                                    children: [
+                                      AppText(
+                                        filteredItem[index]['title'],
+                                        size: 16.sp,
+                                        weight: FontWeight.w500,
+                                      ),
+                                      const Spacer(),
+                                      trailer(
+                                          date: filteredItem[index]
+                                              ["purchase date"],
+                                          text: 'Purchase:')
+                                    ],
+                                  ),
+                                  subtitle: Row(children: [
+                                    AppText(
+                                      ""
+                                      "${filteredItem[index]['quantity'].toString()} ${filteredItem[index]["measureUnit"]} left",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall
+                                          ?.copyWith(fontSize: 11.sp),
+                                    ),
+                                    const Spacer(),
+                                    trailer(
+                                      date: filteredItem[index]["expiry date"],
+                                      text: "Expiry:",
+                                    ),
+                                  ]),
+                                ),
+                                10.h.sbH
+                              ],
+                            ),
+                          );
                         },
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Filterbutton(),
-                          ],
-                        ),
                       ),
                     )
                   ],
                 ),
-              ),
-              15.h.sbH,
-              Expanded(
-                child: ListView.builder(
-                  itemCount: item.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        // Navigator.push(
-                        //     context,
-                        //     MaterialPageRoute(
-                        //         builder: (context) => EditItemScreen()));
-                      },
-                      child: Column(
-                        children: [
-                          ListTile(
-                            title: Row(
-                              children: [
-                                AppText(
-                                  item[index]['title'],
-                                  size: 16.sp,
-                                  weight: FontWeight.w500,
-                                ),
-                                const Spacer(),
-                                trailer(
-                                    date: item[index]["purchase date"],
-                                    text: 'Purchase:')
-                              ],
-                            ),
-                            subtitle: Row(
-                                children: [
-                                  AppText(""
-                                      "${item[index]['quantity'].toString()} ${item[index]["measureUnit"]} left",
-                                    style: Theme.of(context).textTheme.bodySmall?.copyWith(fontSize: 11.sp),
-                                  ),
-                                  const Spacer(),
-                                  trailer(
-                                    date: item[index]["expiry date"],
-                                    text: "Expiry:",
-                                  ),
-                                ]),
-                          ),
-                          10.h.sbH
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              )
-            ],
-          ),
-        )
-    );
+              ));
   }
 }
 
@@ -246,14 +283,17 @@ class trailer extends StatelessWidget {
         children: [
           TextSpan(
             text: "$text ",
-            style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: 11.sp),
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium
+                ?.copyWith(fontSize: 11.sp),
           ),
           TextSpan(
-              text: date,
-              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+            text: date,
+            style: Theme.of(context).textTheme.bodySmall?.copyWith(
                   fontSize: 11.sp,
                   fontWeight: FontWeight.w400,
-              ),
+                ),
           ),
         ],
       ),
--- lib/screens/main/shoppinglist.dart/shoppinglist.dart
+++ lib/screens/main/shoppinglist.dart/shoppinglist.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:grocey_tag/core/constants/pallete.dart';
 import 'package:grocey_tag/utils/widget_extensions.dart';
+import 'package:grocey_tag/widgets/app-card.dart';
 import 'package:grocey_tag/widgets/apptext.dart';
 
+import '../../../core/constants/app_images.dart';
 import '../../../widgets/app_button.dart';
+import '../../../widgets/empty-state.dart';
 import '../../../widgets/scanmodal.dart';
 
 class ShoppingList extends StatefulWidget {
@@ -22,71 +26,79 @@ class _ShoppingListState extends State<ShoppingList> {
     false,
     false,
   ];
+
+  bool isEmpty = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppText(
-                "Shopping List",
-                size: 22.sp,
-                weight: FontWeight.w600,
-              ),
-              AppText(
-                  "This is an automated list of items running low on stocks"),
-            ],
+          title: AppText(
+            "Shopping List",
+            size: 22.sp,
+            weight: FontWeight.w600,
           ),
         ),
-        body: Column(
-          children: [
-            20.h.sbH,
-            Expanded(
-              child: ListView.builder(
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: Checkbox(
-                        activeColor: Colors.green,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            checked[index] = value!;
-                          });
-                        },
-                        value: checked[index],
-                      ),
-                      title: Row(
-                        children: [
-                          AppText(items[index]),
-                          40.w.sbW,
-                          AppText("(0)"),
-                        ],
-                      ),
-                      trailing: SizedBox(
-                          width: 68.sp,
-                          height: 28.sp,
-                          child: AppButton(
-                            onTap: () {
-                              showModalBottomSheet(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return ScanModalwidget(
-                                      itemName: items[index],
-                                      checked: checked[index],
-                                    );
-                                  });
-                            },
-                            isOutline: true,
-                            textColor: Colors.grey.shade400,
-                            text: "Restock",
-                            borderColor: Colors.grey.shade400,
-                            borderRadius: 16.sp,
-                          )),
-                    );
-                  }),
-            )
-          ],
-        ));
+        body: isEmpty
+            ? const EmptyListState(
+                text: "No Item in Shopping List",
+                lottieFile: AppImages.emptyInventory)
+            : Column(
+                children: [
+                  Padding(
+                    padding: 16.sp.padH,
+                    child: const Row(
+                      children: [
+                        Expanded(
+                          child: AppText(
+                              "This is an automated list of items running low on stocks"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  16.sp.sbH,
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                AppText(
+                                  items[index],
+                                  size: 16.sp,
+                                  weight: FontWeight.w600,
+                                ),
+                                40.w.sbW,
+                                AppText("Quantity left: (0)"),
+                              ],
+                            ),
+                            trailing: AppCard(
+                              backgroundColor: whiteColor,
+                              bordered: true,
+                              radius: 30.sp,
+                              borderWidth: 0.6.sp,
+                              expandable: true,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 16.sp, vertical: 10.sp),
+                              onTap: () {
+                                showModalBottomSheet(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return ScanModalwidget(
+                                        itemName: items[index],
+                                        checked: checked[index],
+                                      );
+                                    });
+                              },
+                              child: AppText(
+                                "Restock",
+                              ),
+                            ),
+                          );
+                        }),
+                  )
+                ],
+              ));
   }
 }
